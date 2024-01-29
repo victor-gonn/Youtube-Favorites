798184d0cc242b225d1db20fccc46b74e18c8ea3
João Victor <103864288+victor-gonn@users.noreply.github.com>
2024-01-29 16:32:43 -0300

    add model map

--- lib/models/video-model.dart
+++ lib/models/video-model.dart
@@ -9,12 +9,23 @@ class Video {
   Video({required this.id, required this.title, required this.thumb, required this.channel});
 
   factory Video.fromJson(Map<String, dynamic> json) {
-    return Video(
+    if(json.containsKey("id")) {
+      return Video(
       id: json["id"] ["videoId"],
       title: json["snippet"] ["title"],
       thumb: json["snippet"] ["thumbnails"] ["high"] ["url"],
       channel: json["snippet"] ["channelTitle"]
     );
+    } else { 
+      return Video(
+        id: json["id"], 
+        title: json["title"], 
+        thumb: json["thumb"], 
+        channel: json["channel"],
+        );
+      
+    }
+    
   }
 
   Map<String, dynamic> toJson() {
